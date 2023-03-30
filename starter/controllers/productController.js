7de8ed4c6c79342a7d3ae39726745267bc461e9b
Jesse Carson <jmcarson222@gmail.com>
2023-03-30 12:01:23 -0500

    productController

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -5,25 +5,69 @@ const {
   NotFoundError,
   UnauthenticatedError,
 } = require("../errors");
+const { response } = require("express");
+const { findOneAndRemove } = require("../models/Product");
 
 const getAllProducts = async (req, res) => {
-  res.send("getAllProducts");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 const getSingleProduct = async (req, res) => {
-  res.send("getSingleProduct");
+  const {
+    params: { id },
+  } = req;
+
+  const singleProduct = await Product.findOne({ _id: id });
+
+  res.status(StatusCodes.OK).json({ singleProduct });
 };
 
 const createProduct = async (req, res) => {
-  res.send("createProduct");
+  req.body.user = req.user.userId;
+
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const updateProduct = async (req, res) => {
-  res.send("updateProduct");
+  const {
+    body: { name, company, price },
+    params: { id },
+    user: { userId },
+  } = req;
+
+  if (!name || !company || !price) {
+    throw new BadRequestError("Provide name, company, and price values");
+  }
+
+  const product = await Product.findOneAndUpdate(
+    { _id: id, user: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!product) {
+    throw new BadRequestError("Product does not exist");
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const deleteProduct = async (req, res) => {
-  res.send("deleteProduct");
+  const {
+    user: { userId },
+    params: { id },
+  } = req;
+
+  const product = await Product.findOneAndRemove({ _id: id, user: userId });
+
+  if (!product) {
+    throw new BadRequestError("Product does not exist");
+  }
+
+  res.status(StatusCodes.OK).send(`${product} has been removed`);
 };
 
 const uploadImage = async (req, res) => {
