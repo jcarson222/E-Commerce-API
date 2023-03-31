35e0610f81bddc215c3532c4cd329240adac5612
Jesse Carson <jmcarson222@gmail.com>
2023-03-31 10:10:27 -0500

    createReview

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -8,7 +8,7 @@ const {
   UnauthenticatedError,
 } = require("../errors");
 
-const {checkPermissions} = require('../utils')
+const { checkPermissions } = require("../utils");
 
 const getAllReviews = async (req, res) => {
   res.send("get all reviews");
@@ -19,7 +19,34 @@ const getSingleReview = async (req, res) => {
 };
 
 const createReview = async (req, res) => {
-  req.body.product = 
+  // CHECK IF PRODUCT EXISTS
+  const { product: productId } = req.body;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new NotFoundError(`Product with id: ${productId} does not exist`);
+  }
+
+  // CHECK IF REVIEW ALREADY SUBMITTED BY USER
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new BadRequestError(
+      "You have already submitted a review for this project"
+    );
+  }
+
+  // GET USER TO REQ.BODY
+  req.body.user = req.user.userId;
+
+  // CREATE REVIEW
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const updateReview = async (req, res) => {
